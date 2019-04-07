5b8832a4d259a08e246ca1f050bec533f6225aed
lily <physalily@gmail.com>
2019-04-08 02:16:44 +0900

    add generater function (gen)

--- 9cc.c
+++ 9cc.c
@@ -109,6 +109,42 @@ void error(int i)
 	exit(1);
 }	
 
+void gen(Node *node)
+{
+	//if node is number, node value push in the register
+	if(node->ty == ND_NUM)
+	{
+		printf("	push %d\n", node->val);
+		return;
+	}
+	
+	//Recursion gen,
+	gen(node->lhs);
+	gen(node->rhs);
+
+	//operate, pop and calc
+	printf("	pop rdi\n");
+	printf("	pop rax\n");
+
+	switch(node->ty)
+	{
+		case '+':
+			printf("	add rax, rdi\n");
+			break;
+		case '-':
+			printf("	sub rax, rdi\n");
+			break;
+		case '*':
+			printf("	mul rdi\n");
+			break;
+		case '/':
+			printf("	mov rdx, 0\n");
+			printf("	div rdi\n");
+	}
+
+	printf("	push rax\n");
+}
+
 int main(int argc, char **argv)
 {
 	if(argc != 2)
